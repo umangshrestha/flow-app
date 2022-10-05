c6a40ee3430919cc9c57af6a45b6855159f3e37f
Umang Shrestha <umang@lifesignals.com>
2022-10-05 12:46:38 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.useGlobalFilters(new PrismaClientKnownRequestFilter);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT||3000);
 }
 
 bootstrap()
